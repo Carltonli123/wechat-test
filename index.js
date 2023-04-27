7b21a06dea17ab75f234844d90067c223a8b6503
Carlton Li <gregor.goff@gmail.com>
2023-04-27 14:11:04 +0800

    added something

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Welcome to Adyen\'s wechat portal!')
 })
 
 app.listen(port, () => {
