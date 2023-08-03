6379526b30be05fcd1203e185be8dca6ad151bf2
Carlton <gregor.goff@gmail.com>
2023-08-03 12:08:41 +0800

    today is Aug 3rd

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const port = 80
 
 app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to Adyen\'s wechat portal!')
+  res.send('Hello World! Welcome to Adyen\'s wechat portal! \n today is 2022/08/03 !')
 })
 
 app.listen(port, () => {
