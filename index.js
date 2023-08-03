41ec29393278a73e3b7da13728f90531b3f43a02
Carlton Li <gregor.goff@gmail.com>
2023-08-03 12:37:39 +0800

    commited on local

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-
 const express = require('express')
 const app = express()
 const port = 80
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello World! Welcome to Adyen\'s wechat portal!')
+  res.send('Hello World! Welcome to Adyen\'s wechat portal! it will be modified today!')
 })
 
 app.listen(port, () => {
