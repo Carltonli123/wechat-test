86e46f3be298ea50d1abb9041fa97dc4dd7d6673
Carlton Li <gregor.goff@gmail.com>
2023-08-03 17:06:50 +0800

    added logs

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
 const port = 80
-var bodyParser = require("body-parser")
+const bodyParser = require("body-parser")
+const fs = require('fs')
+const path = require('path')
 
 //view engine
 app.set('view engine', 'ejs');
+
+//serve files in the "public" foler
+app.use(express.static('public'))
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -22,10 +27,59 @@ app.get('/netease-game-club', (req, res) => {
 
 //webhook
 app.post('/adyen/notify',(req,res) =>{
-     console.log(JSON.parse(req.body));
+
+     if(checkIfIsTransEvent(req.body.notificationItems[0].NotificationRequestItem.eventCode)){
+         writeToLogs(__dirname + "/views/logs/transactionEvents.log",req.body);
+     }else if(checkIfIsDisputeEvent(req.body.notificationItems[0].NotificationRequestItem.eventCode)){
+          writeToLogs(__dirname + "/views/logs/disputeEvents.log",req.body);
+     }else if(checkIfIsPayoutEvent(req.body.notificationItems[0].NotificationRequestItem.eventCode)){
+          writeToLogs(__dirname + "/views/logs/payoutEvents.log",req.body);
+     }else{
+          writeToLogs(__dirname + "/views/logs/otherEvents.log",req.body);
+     };
+ 
+     //response
      res.send("[accepted]" );
 })
 
+// show the logs
+app.get('/translogs',(req,res) =>{
+  truncateFile(__dirname + "/views/logs/transactionEvents.log");
+  fs.readFile(__dirname + "/views/logs/transactionEvents.log", 'utf8', function(err, data){
+    res.render('logs/logPage',{ data : data });
+    // Display the file content
+    console.log(data);
+  });
+})
+
+app.get('/disputelogs',(req,res) =>{
+  truncateFile(__dirname + "/views/logs/disputeEvents.log");
+  fs.readFile(__dirname + "/views/logs/disputeEvents.log", 'utf8', function(err, data){
+    res.render('logs/logPage',{ data : data });
+    // Display the file content
+    console.log(data);
+  });
+})
+
+app.get('/payoutlogs',(req,res) =>{
+  truncateFile(__dirname + "/views/logs/payoutEvents.log");
+  fs.readFile(__dirname + "/views/logs/payoutEvents.log", 'utf8', function(err, data){
+    res.render('logs/logPage',{ data : data });
+    // Display the file content
+    console.log(data);
+  });
+})
+
+app.get('/otherlogs',(req,res) =>{
+  truncateFile(__dirname + "/views/logs/otherEvents.log");
+  fs.readFile(__dirname + "/views/logs/otherEvents.log", 'utf8', function(err, data){
+    res.render('logs/logPage',{ data : data });
+    // Display the file content
+    console.log(data);
+  });
+})
+
+//check if webhook is working
 app.get('/adyen/notify',(req,res) =>{
   res.send('the webhook is working');
 })
@@ -33,3 +87,47 @@ app.get('/adyen/notify',(req,res) =>{
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+//miscellenious functions
+function checkIfIsTransEvent(event){
+  if(event='AUTHORISATION'||'CAPTURE'||'REFUND'){
+    return true;
+  }else{
+    return false;
+ }
+};
+
+function checkIfIsDisputeEvent(event){
+  if(event='CHARGEBACK'||'NOTIFICATION_OF_CHARGEBACK'||'NOTIFICATION_OF_FRAUD'){
+    return true;
+  }else{
+    return false;
+ }
+};
+
+function checkIfIsPayoutEvent(event){
+  if(event='PAYOUT_EXPIRE'||'PAYOUT_DECLINE'||'PAIDOUT_REVERSED'){
+    return true;
+  }else{
+    return false;
+ }
+};
+
+function writeToLogs(path, data){
+          //write log files
+          fs.appendFile(path, JSON.stringify(data, null, 4)+"\r\n\r\n\r\n\r\n\r\n", function (err) {
+            if (err) throw err;
+            console.log('Webhook message has been saved to log file');
+          });
+}
+
+function truncateFile(fileName){
+  fs.readFile(fileName, 'utf8', function(err, data){
+    if (err){
+        throw err;
+    }
+    var linesCount = data.split("\n").length;
+    var wantedLines = data.split("\n").slice(~240+1);//keep 240 lines
+    fs.writeFile(fileName, wantedLines.join(), (err) => err && console.error(err));
+});
+}
